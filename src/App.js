50c78997721fe8ffbd21045bbf7be69169d9658b
Chiraag1304 <93964021+Chiraag1304@users.noreply.github.com>
2023-11-23 11:38:22 +0530

    Update App.js
    Created a from

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import './App.css';
 
 function App() {
+  // Define the initial form state
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    SRN: '',
+  });
+
+  // Handle form input changes
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const isConfirmed = window.confirm('Are you sure you want to submit the form?');
+    if (isConfirmed) {
+      
+      console.log('Form data submitted:', formData);
+    
+    }
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-          hello world 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Form Example</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formFirstName">
+          <Form.Label>First Name:</Form.Label>
+          <Form.Control
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="formLastName">
+          <Form.Label>Last Name:</Form.Label>
+          <Form.Control
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="formSRN">
+          <Form.Label>SRN:</Form.Label>
+          <Form.Control
+            type="text"
+            name="SRN"
+            value={formData.SRN}
+            onChange={handleInputChange}
+          />
+        </Form.Group>
+        <Button variant="success" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
